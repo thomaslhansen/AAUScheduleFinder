a647d174aa932ac52ff2cb672d37ddd5f7234f5f
Thomas Lundsgaard Hansen <thomaslhansen@live.dk>
2020-08-18 09:24:30 +0200

    Added a progressbar and finishing touches

--- AAUScheduleFinder.sh
+++ AAUScheduleFinder.sh
@@ -6,9 +6,10 @@
 #####################################
 
 #!/bin/bash
+clear
 
 # Print a welcome
-printf "Velkommen til AAUScheduleFinder!\nA simple tool to find your future schedule at AAU\n"
+printf "Velkommen til AAUScheduleFinder!\nEt simplet værktøj til at finde din nye semester kalender ved AAU\n"
 
 
 # Prompt for the name of the calendar
@@ -22,14 +23,18 @@ read index
 # Alert the user that the search has started
 printf "\nSøgningen er nu igang, vi giver besked hvis vi finder noget!\n"
 
+pstr="[=======================================================================]"
+
 # For any id from the one specified by the user to max id 9999 do a wget on the calendar url and
 # grep for the calendar name. If found the user will be alerted.
 for i in `seq $index 9999`;
 do
+pd=$(( ($i - $index ) * 73 /( 9999 - $index ) ))
+printf "\r%3d.%1d%% %.${pd}s" $(( ($i - $index ) * 100 / ( 9999 - $index ) )) $(( (( $i - $index ) * 1000 / ( 9999 - $index )) % 10 )) $pstr
 if wget -qO- https://www.moodle.aau.dk/calmoodle/public/\?sid\=$i | grep -q "$search"; then
-    echo "Vi fandt en kalender på https://www.moodle.aau.dk/calmoodle/public/?sid=$i der passer dine søgekriterier."
-    echo "Vi forsætter søgningen i tilfælde at der er flere kalendre der passer"
-    notify-send AAUSchedulFinder "Vi fandt en kalender"
+    echo "\nVi fandt en kalender på https://www.moodle.aau.dk/calmoodle/public/?sid=$i der passer dine søgekriterier."
+    echo "Vi forsætter søgningen i det tilfælde, at der er flere kalendre der passer til dine søgekriterier"
+    notify-send AAUSchedulFinder "Kalender fundet!"
 fi
 done
 
